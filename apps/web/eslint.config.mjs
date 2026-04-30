c285cb391345a6c2f6a25190d35c75f39bf1fde1
Reja Putra Perdana <rejaputraperdana@gmail.com>
2026-04-30 08:53:38 +0700

    fix: simplify eslint config (remove @next/eslint-plugin-next dep)

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,18 +1,9 @@
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
-import nextPlugin from "@next/eslint-plugin-next";
 
 export default [
   js.configs.recommended,
   ...tseslint.configs.recommended,
-  {
-    plugins: {
-      "@next/next": nextPlugin,
-    },
-    rules: {
-      ...nextPlugin.configs.recommended.rules,
-    },
-  },
   {
     // Allow CommonJS in config files and service worker files
     ignores: [
